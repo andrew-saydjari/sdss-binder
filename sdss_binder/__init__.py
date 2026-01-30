578174029b14120375662ec3ab9910b39b5027ef
Andy Casey <andycasey@mini.local>
2026-01-30 18:20:23 +1100

    Incorporate jupyter-marimo-proxy directly, launch from notebooks/marimo/

--- sdss_binder/__init__.py
+++ sdss_binder/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import os
+import base64
+import secrets
 
 def setup():
 	def mappath(path):
@@ -11,5 +13,27 @@ def setup():
 		'launcher_entry': {
 			'title': 'SDSS and BinderHub',
 			'icon_path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'sdss.svg')
+		},
+	}
+
+def setup_marimo():
+	token = secrets.token_urlsafe(16)
+	return {
+		'command': [
+			'marimo', 'edit',
+			'--port', '{port}',
+			'--base-url', os.environ['JUPYTERHUB_SERVICE_PREFIX'] + 'marimo',
+			'--token', '--token-password', token,
+			'--headless',
+			'notebooks/marimo'
+		],
+		'timeout': 60,
+		'absolute_url': True,
+		'request_headers_override': {
+			'Authorization': 'Basic ' + base64.b64encode(b' :' + token.encode()).decode()
+		},
+		'launcher_entry': {
+			'title': 'Marimo',
+			'icon_path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'marimo.svg')
 		},
 	}
